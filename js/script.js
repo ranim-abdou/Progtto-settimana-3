4d09d3245c4e8c04549cf24ff913fbd77685a9a4
ranim-abdou <rany99s5@gmail.com>
2022-05-15 12:44:11 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+function display(numeber){
+    document.getElementById('risultato').value += Number;
+}
+
+/*function risultato(){
+    let res_str = document.getElementById('risultato').value;
+    let res_arr;
+    if(res_str.indexOf('+')>-1){
+        res_arr = res_str.split("+");
+        let totale = (parseFloat(res_arr[0]))+(parseFloat(res_arr[1]));
+        document.getElementById('risultato').value = totale;
+    }
+
+    else if(res_str.indexOf('-') > -1){
+        res_arr = res_str.split("-");
+        let totale = (parseFloat(res_arr[0]))-(parseFloat(res_arr[1]));
+        document.getElementById('risultato').value = totale;
+    }
+    else if(res_str.indexOf('*') > -1){
+        res_arr = res_str.split("*");
+        let totale = (parseFloat(res_arr[0]))*(parseFloat(res_arr[1]));
+        document.getElementById('risultato').value = totale;
+    }
+    else if(res_str.indexOf('/') > -1){
+        res_arr = res_str.split("/");
+        let totale = (parseFloat(res_arr[0]))/(parseFloat(res_arr[1]));
+        document.getElementById('risultato').value = totale;
+    }
+    else {document.getElementById('risultato').value = 'Non ci sono operatori inseriti'}
+    console.log(res_arr);
+}
+
+
+function reset(){
+    document.getElementById('risultato').value = '';
+}*/
+
+function operazione() { 
+    document.getElementById("operazioni").value=eval(document.getElementById("operazioni").value); 
+    }
+    
+    function reset() { 
+     document.getElementById("operazioni").value=""; 
+    }
+    
+
+    
